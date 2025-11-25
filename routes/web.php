3e677948a43816e866b9125225e3bf872becc473
Youness SBAI <youness.sbai.work@gmail.com>
2025-11-26 00:35:11 +0100

    Setup Laravel 12

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/test', function () {
-    return 'this is a test route';
-});
-
 Route::get('/', function () {
     return Inertia::render('welcome', [
         'canRegister' => Features::enabled(Features::registration()),
